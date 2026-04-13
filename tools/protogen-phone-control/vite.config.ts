b3118e59a04afbfcb57e8ffcfe0e66396221286e
Exora02 <antoine.leyvraz@gmail.com>
2026-04-13 13:18:03 +0200

    fix: simplify URL matching for IP registration and retrieval in discoverPlugin

--- tools/protogen-phone-control/vite.config.ts
+++ tools/protogen-phone-control/vite.config.ts
@@ -8,8 +8,11 @@ const discoverPlugin = {
   name: 'protogen-discover',
   configureServer(server) {
     server.middlewares.use((req, res, next) => {
+      // Remove query parameters from URL matching
+      const urlPath = req.url.split('?')[0];
+
       // ESP32 hits this to register its IP
-      if (req.url.startsWith('/api/register?ip=')) {
+      if (urlPath === '/api/register') {
         const ip = req.url.split('ip=')[1];
         if (ip) {
           protogenIp = `http://${ip}`;
@@ -20,7 +23,7 @@ const discoverPlugin = {
         }
       }
       // Control panel hits this to get the IP
-      if (req.url === '/api/get-ip') {
+      if (urlPath === '/api/get-ip') {
         res.statusCode = 200;
         res.setHeader('Content-Type', 'application/json');
         res.end(JSON.stringify({ ip: protogenIp }));
